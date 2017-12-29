6453edd82861b0878725232d803f66e50a90d071
Gaurav Tiwari <tiwarigaurav1998@gmail.com>
2017-12-30 03:48:13 +0530

    Update query.js

--- Lecture17/mongodb-example/query.js
+++ Lecture17/mongodb-example/query.js
@@ -1,23 +1,50 @@
-const MongoClient = require('mongodb').MongoClient;
+// to start mongodb server, in terminal, create mongodb folder and => mongod --dbpath=./mongodb
 
-const dbUri = 'mongodb://localhost:27017/exampledb'
+const MongoClient=require('mongodb').MongoClient;
 
-MongoClient.connect(dbUri, function (err, db) {
-    if (err) throw err;
+const url='mongodb://localhost:27017/exampledb';//connect to database
 
-    const todos = db.collection('todos')
-    let todoitems = todos.find({
-        $and: [
-            {priority: {$lt: 4}},
-            {priority: {$gt: 1}}
+MongoClient.connect(url,function (err,db) {
+
+    if(err)
+        throw err;
+
+    const todos=db.collection('todos');  //get collection todos. Creates it if doesn't exist
+
+
+    //
+    // let todoItems=todos.find({}); //todoItems is of type cursor. go to debug mode to see its properties.
+    // // let todoItems=todo.find({done:true}); will give those whose done is true
+    // todoItems.toArray((err,results)=>{
+    //         if(err)
+    //             throw err;
+    //         console.log(results);
+    //         // db.close();
+    // });
+
+    let todoItems=todos.find({
+        // priority:{
+        //     $gt:2 //for priority >2
+        // }
+
+        $and:[  //priority >0 and <4
+            {priority:{$gt:0}},
+            {priority:{$lt:4}}
         ]
-    })
-    todoitems.toArray(function (err, items) {
-        if (err) throw  err;
 
-        console.log(items)
+    })
+    todoItems.toArray((err,results)=>{
+        if(err){
+            throw err;
+        }
+
+        console.log(results);
         db.close();
     })
 
 
+
 })
+
+
+
